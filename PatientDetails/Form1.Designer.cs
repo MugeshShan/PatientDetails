66754223af7aa8b595e2c25fbd785e888e1597cf
Mugesh Shanmugavel <mugesh.shanmugavel@justanswer.com>
2021-12-10 22:55:37 +0530

    Change in layout

--- PatientDetails/Form1.Designer.cs
+++ PatientDetails/Form1.Designer.cs
@@ -58,9 +58,10 @@
             // 
             // button10
             // 
-            this.button10.Location = new System.Drawing.Point(362, 554);
+            this.button10.Location = new System.Drawing.Point(573, 205);
+            this.button10.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.Size = new System.Drawing.Size(112, 35);
             this.button10.TabIndex = 50;
             this.button10.Text = "Search";
             this.button10.UseVisualStyleBackColor = true;
@@ -68,9 +69,10 @@
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(776, 674);
+            this.button9.Location = new System.Drawing.Point(994, 205);
+            this.button9.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.Size = new System.Drawing.Size(112, 35);
             this.button9.TabIndex = 49;
             this.button9.Text = "Get";
             this.button9.UseVisualStyleBackColor = true;
@@ -79,16 +81,19 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(509, 88);
+            this.dataGridView1.Location = new System.Drawing.Point(33, 265);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(562, 452);
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(1467, 410);
             this.dataGridView1.TabIndex = 48;
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(968, 596);
+            this.button8.Location = new System.Drawing.Point(1548, 630);
+            this.button8.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(103, 23);
+            this.button8.Size = new System.Drawing.Size(174, 35);
             this.button8.TabIndex = 47;
             this.button8.Text = "Last";
             this.button8.UseVisualStyleBackColor = true;
@@ -96,9 +101,10 @@
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(827, 596);
+            this.button7.Location = new System.Drawing.Point(1548, 527);
+            this.button7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(104, 23);
+            this.button7.Size = new System.Drawing.Size(174, 35);
             this.button7.TabIndex = 46;
             this.button7.Text = "Next";
             this.button7.UseVisualStyleBackColor = true;
@@ -106,9 +112,10 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(664, 596);
+            this.button6.Location = new System.Drawing.Point(1548, 405);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(116, 23);
+            this.button6.Size = new System.Drawing.Size(174, 35);
             this.button6.TabIndex = 45;
             this.button6.Text = "Prev";
             this.button6.UseVisualStyleBackColor = true;
@@ -116,9 +123,10 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(509, 596);
+            this.button5.Location = new System.Drawing.Point(1548, 292);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(116, 23);
+            this.button5.Size = new System.Drawing.Size(174, 35);
             this.button5.TabIndex = 44;
             this.button5.Text = "First";
             this.button5.UseVisualStyleBackColor = true;
@@ -127,18 +135,20 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(146, 231);
+            this.label7.Location = new System.Drawing.Point(29, 131);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(42, 13);
+            this.label7.Size = new System.Drawing.Size(63, 20);
             this.label7.TabIndex = 43;
             this.label7.Text = "Gender";
             // 
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(362, 229);
+            this.radioButton2.Location = new System.Drawing.Point(282, 131);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(59, 17);
+            this.radioButton2.Size = new System.Drawing.Size(87, 24);
             this.radioButton2.TabIndex = 42;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Female";
@@ -147,9 +157,10 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(256, 229);
+            this.radioButton1.Location = new System.Drawing.Point(152, 129);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(48, 17);
+            this.radioButton1.Size = new System.Drawing.Size(68, 24);
             this.radioButton1.TabIndex = 41;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Male";
@@ -157,9 +168,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(256, 554);
+            this.button4.Location = new System.Drawing.Point(400, 205);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(112, 35);
             this.button4.TabIndex = 40;
             this.button4.Text = "Delete";
             this.button4.UseVisualStyleBackColor = true;
@@ -167,9 +179,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(306, 608);
+            this.button3.Location = new System.Drawing.Point(778, 205);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(112, 35);
             this.button3.TabIndex = 39;
             this.button3.Text = "Reset";
             this.button3.UseVisualStyleBackColor = true;
@@ -177,9 +190,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(362, 491);
+            this.button2.Location = new System.Drawing.Point(212, 205);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(112, 35);
             this.button2.TabIndex = 38;
             this.button2.Text = "Update";
             this.button2.UseVisualStyleBackColor = true;
@@ -187,9 +201,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(256, 491);
+            this.button1.Location = new System.Drawing.Point(33, 205);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(112, 35);
             this.button1.TabIndex = 37;
             this.button1.Text = "Add";
             this.button1.UseVisualStyleBackColor = true;
@@ -197,82 +212,92 @@
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(256, 438);
+            this.textBox5.Location = new System.Drawing.Point(1452, 135);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(181, 20);
+            this.textBox5.Size = new System.Drawing.Size(270, 26);
             this.textBox5.TabIndex = 36;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(256, 387);
+            this.textBox4.Location = new System.Drawing.Point(1452, 76);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(181, 20);
+            this.textBox4.Size = new System.Drawing.Size(270, 26);
             this.textBox4.TabIndex = 35;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(256, 302);
+            this.textBox3.Location = new System.Drawing.Point(1006, 76);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(181, 59);
+            this.textBox3.Size = new System.Drawing.Size(270, 89);
             this.textBox3.TabIndex = 34;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(256, 150);
+            this.textBox2.Location = new System.Drawing.Point(584, 79);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(181, 20);
+            this.textBox2.Size = new System.Drawing.Size(270, 26);
             this.textBox2.TabIndex = 33;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(256, 88);
+            this.textBox1.Location = new System.Drawing.Point(152, 76);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(181, 20);
+            this.textBox1.Size = new System.Drawing.Size(270, 26);
             this.textBox1.TabIndex = 32;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(146, 387);
+            this.label6.Location = new System.Drawing.Point(1311, 79);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(64, 13);
+            this.label6.Size = new System.Drawing.Size(95, 20);
             this.label6.TabIndex = 31;
             this.label6.Text = "Postal Code";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(146, 438);
+            this.label5.Location = new System.Drawing.Point(1311, 135);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(78, 13);
+            this.label5.Size = new System.Drawing.Size(115, 20);
             this.label5.TabIndex = 30;
             this.label5.Text = "Phone Number";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(146, 302);
+            this.label3.Location = new System.Drawing.Point(902, 85);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.Size = new System.Drawing.Size(68, 20);
             this.label3.TabIndex = 29;
             this.label3.Text = "Address";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(142, 150);
+            this.label4.Location = new System.Drawing.Point(440, 82);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.Size = new System.Drawing.Size(105, 20);
             this.label4.TabIndex = 28;
             this.label4.Text = "Patient Name";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(142, 91);
+            this.label2.Location = new System.Drawing.Point(29, 79);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.Size = new System.Drawing.Size(80, 20);
             this.label2.TabIndex = 27;
             this.label2.Text = "Patient ID";
             // 
@@ -280,17 +305,18 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(139, 50);
+            this.label1.Location = new System.Drawing.Point(28, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(114, 17);
+            this.label1.Size = new System.Drawing.Size(151, 25);
             this.label1.TabIndex = 26;
             this.label1.Text = "Patient Details";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1211, 747);
+            this.ClientSize = new System.Drawing.Size(1806, 730);
             this.Controls.Add(this.button10);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.dataGridView1);
@@ -316,6 +342,7 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
